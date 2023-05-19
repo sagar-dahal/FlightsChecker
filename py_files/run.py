9669be8a09ccec8f70d377036b287668311064ba
Sagar Dahal <dahal25sagar@gmail.com>
2023-05-18 20:29:12 -0400

    Added argparse. CLI execution works as expected.

--- py_files/run.py
+++ py_files/run.py
@@ -1,13 +1,20 @@
 from QatarAirways.qatarAirways import QatarAirways
+import argparse
 
-def getFromQatar():
+def getFromQatar(origin, destination, date):
     qatar = QatarAirways()
     qatar.land_first_page()
-    qatar.enterOrigin('new york')
-    qatar.enterDestination('kathmandu')
+    qatar.enterOrigin(origin)
+    qatar.enterDestination(destination)
     qatar.enterTripType()
-    qatar.departDate('2023-08-05')
+    qatar.departDate(date)
     qatar.showFlights()
     return qatar.getResult()
 
-print(getFromQatar())
+parser = argparse.ArgumentParser(description='Get flight info for a particular date')
+parser.add_argument('origin', type=str, help='Put the departure location or airport')
+parser.add_argument('destination', type=str, help='Put the arrival location or airport')
+parser.add_argument('date', type=str, help='Put the departure date in the format yyyy-mm-dd')
+args = parser.parse_args()
+
+print(getFromQatar(args.origin, args.destination, args.date))
